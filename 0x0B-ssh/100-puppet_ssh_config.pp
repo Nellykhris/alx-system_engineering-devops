b35689d7f9c7f77c356ce8db2550338ed3583f48
Nellykhris <ifeomaomeonu@gmail.com>
2022-10-19 18:09:42 +0100

    Comit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# make changes to ssh_config file
+
+file_line {'set PasswordAuthentication to no':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+
+file_line {'use school as private key auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
