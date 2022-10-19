59503931e53446be17f41ff9ad310044507ae5bd
Nellykhris <ifeomaomeonu@gmail.com>
2022-10-19 17:32:50 +0100

    Commit 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # create a file in /tmp
-file {'/tmp/holberton':
-path    => '/tmp/holberton',
+file {'/tmp':
+path    => '/tmp/school',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
 content => 'I love Puppet'
 }
-
